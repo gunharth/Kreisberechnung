bc18cb45c648571edaf0c7f36b272b659902a7f5
A409.01 <A409.01@A409-01>
2021-11-30 21:20:57 +0100

    final

--- Kreisberechnung/Program.cs
+++ Kreisberechnung/Program.cs
@@ -7,9 +7,16 @@ namespace Kreisberechnung
         static void Main(string[] args)
         {
             Kreis kreis1 = new Kreis(10);
+            Console.WriteLine("Kreis1");
             Console.WriteLine("Durchmesser: " + kreis1.Durchmesser());
             Console.WriteLine("Umfang: " + kreis1.Umfang());
             Console.WriteLine("Fläche: " + kreis1.Flaeche());
+
+            Console.WriteLine("Kreis1");
+            Kreis kreis2 = new Kreis(90.40);
+            Console.WriteLine("Durchmesser: " + kreis2.Durchmesser());
+            Console.WriteLine("Umfang: " + kreis2.Umfang());
+            Console.WriteLine("Fläche: " + kreis2.Flaeche());
         }
     }
 
