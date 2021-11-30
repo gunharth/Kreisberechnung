e150b1ca0cfc6f2581e4605b30d9702c08ba70e3
A409.01 <A409.01@A409-01>
2021-11-30 21:16:20 +0100

    Projektdateien hinzufügen.

---
+++ Kreisberechnung/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Kreisberechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kreis kreis1 = new Kreis(10);
+            Console.WriteLine("Durchmesser: " + kreis1.Durchmesser());
+            Console.WriteLine("Umfang: " + kreis1.Umfang());
+            Console.WriteLine("Fläche: " + kreis1.Flaeche());
+        }
+    }
+
+    class Kreis
+    {
+        public double Radius; // Eigenschaft
+
+        public Kreis(double radius) // Konstruktor
+        {
+            Radius = radius;
+        }
+
+        // Methoden
+        public double Durchmesser()
+        {
+            return Radius * 2;
+        }
+
+        public double Umfang()
+        {
+            return 2 * Radius * Math.PI;
+        }
+
+        public double Flaeche()
+        {
+            return this.Radius * this.Radius * Math.PI; // this ist nicht verpflichtend - schützt aber vor Verwechslung - this referenziert immer das Object selbst
+        }
+    }
+}
